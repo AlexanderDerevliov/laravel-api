815c8d7c7fe7408c8da892b0a505fe42d4f73da2
AlexanderDerevliov <alex.feelred@gmail.com>
2024-02-08 20:20:10 +0100

    tested api on postman

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Admin\PostController;
+use App\Http\Controllers\Api\PostController as ApiPostController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +16,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::get('/test', function() {
+    return response()->json([
+        'name' => 'Alex',
+        'state' => 'Italy',
+    ]);
 });
+
+Route::get('/posts', [ApiPostController::class, 'index']);
